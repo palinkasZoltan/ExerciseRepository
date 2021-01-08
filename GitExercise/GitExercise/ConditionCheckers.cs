e1fedeb506c2a1c06086a06f2496405edb9ea5a8
palinkasZoltan <77148226+palinkasZoltan@users.noreply.github.com>
2021-01-08 11:19:40 +0100

    I made the function for the even number checking within it's own class.

---
+++ GitExercise/GitExercise/ConditionCheckers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitExercise
+{
+    class ConditionCheckers
+    {
+        public bool IsItEven(int number) => number % 2 == 0;
+    }
+}
