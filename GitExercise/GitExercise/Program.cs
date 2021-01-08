8bcf5de46f764ea408b87cc1d9ea5319eb5c41fd
palinkasZoltan <77148226+palinkasZoltan@users.noreply.github.com>
2021-01-08 11:19:58 +0100

    I made the "UI" for the even number checking.

--- GitExercise/GitExercise/Program.cs
+++ GitExercise/GitExercise/Program.cs
@@ -6,7 +6,21 @@ namespace GitExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Give in a number please : ");
+            ConditionCheckers conditionC = new ConditionCheckers();
+            int nr = 0;
+            while (!int.TryParse(Console.ReadLine(), out nr))
+            {
+                Console.WriteLine("Give in a correct value!");
+            }
+            if (conditionC.IsItEven(nr))
+            {
+                Console.WriteLine("This number is even.");
+            }
+            else
+            {
+                Console.WriteLine("This number is not even!");
+            }
         }
     }
 }
