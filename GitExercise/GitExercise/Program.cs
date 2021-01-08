c8a7f942d9c01b820fbafa503e9de1630b1b6116
palinkasZoltan <77148226+palinkasZoltan@users.noreply.github.com>
2021-01-08 11:26:31 +0100

    I exported everything from the Main to a new class to make it clearer.

--- GitExercise/GitExercise/Program.cs
+++ GitExercise/GitExercise/Program.cs
@@ -6,21 +6,7 @@ namespace GitExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Give in a number please : ");
-            ConditionCheckers conditionC = new ConditionCheckers();
-            int nr = 0;
-            while (!int.TryParse(Console.ReadLine(), out nr))
-            {
-                Console.WriteLine("Give in a correct value!");
-            }
-            if (conditionC.IsItEven(nr))
-            {
-                Console.WriteLine("This number is even.");
-            }
-            else
-            {
-                Console.WriteLine("This number is not even!");
-            }
+            Launcher.Launch();
         }
     }
 }
